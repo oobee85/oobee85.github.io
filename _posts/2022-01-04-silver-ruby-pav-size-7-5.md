0304c2ed0ade5beeeda99b212fb0409bcd735445
Oleks <oleks.korshak@gmail.com>
2022-01-05 00:12:49 -0800

    selection maybe works?

--- _posts/2022-01-04-silver-ruby-pav-size-7-5.md
+++ _posts/2022-01-04-silver-ruby-pav-size-7-5.md
@@ -2,6 +2,7 @@
 layout: post
 published: true
 title: Silver Ruby Pavé Size 7.5
+category: pave
 ---
 ![pave_silver_ruby_8-0.jpg](/images/jewelry/rings/pave_silver_ruby_8-0.jpg)
 <!--more-->
--- assets/js/main.js
+++ assets/js/main.js
@@ -2,4 +2,29 @@
 function selectCategory(category) {
     console.log("asdf");
     console.log(category);
+    select(category);
+}
+
+
+
+let selected;
+function select(category) {
+
+    // Highlight the button in the nav bar
+//   document.getElementById(id).className = "category selected";
+//   selected = id;
+
+  //Show and hide the projects deppending on if they contain the category or not
+  let allposts = document.getElementsByClassName("post");
+  let categoryposts = document.getElementsByClassName(category);
+
+  for (let i = 0; i < allposts.length; i++) {
+    let post = allposts[i];
+    if (post.classList.contains(category)) {
+        project.style.display = "";
+    }
+    else {
+        project.style.display = "none";
+    }
+  }
 }
